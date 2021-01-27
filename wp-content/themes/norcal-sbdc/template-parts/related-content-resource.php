4a62dc71eec96dd98571bf332be9361f5f4851dd
Cameron Rahman <cameron.rahman@gmail.com>
2021-01-27 08:48:56 -0800

    Adding related resources to end of resource page

---
+++ wp-content/themes/norcal-sbdc/template-parts/related-content-resource.php
@@ -0,0 +1,18 @@
+
+
+<?php $related_post_ids = ct_get_related_posts( get_the_ID(), array( 'post_topic' => 1 ), array( 'fields' => 'ids', 'post_type' => 'resource' ) ); ?>
+<?php if ( ! empty( $related_post_ids ) && count( $related_post_ids ) >= 1 ) { ?>
+	<div id="related-content">
+		<h3 class="related-content-title">Related Resources</h3>
+		<?php
+			$block = array(
+				'blockName' => 'crown-blocks/featured-resource-slider',
+				'attrs' => array(
+					'manuallySelectPosts' => true,
+					'filterPostsInclude' => array_map( function ( $n ) { return array( 'id' => $n ); }, $related_post_ids )
+				)
+			);
+			echo render_block( $block );
+		?>
+	</div>
+<?php } ?>
