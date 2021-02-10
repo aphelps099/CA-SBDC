d554698bb0c5027d33411bb062a1d64ca14dfedd
Cameron Rahman <cameron.rahman@gmail.com>
2021-02-10 08:58:45 -0800

    Styling base search results

--- wp-content/themes/norcal-sbdc/template-parts/index-entry-content.php
+++ wp-content/themes/norcal-sbdc/template-parts/index-entry-content.php
@@ -1,18 +1,17 @@
 <article <?php post_class(); ?>>
 
-	<div class="entry-thumbnail">
-		<div class="inner">
-			<a href="<?php the_permalink(); ?>">
-				<?php ct_bg_image_css( get_post_thumbnail_id(), '#index-entries .post-' . get_the_ID() . ' .entry-thumbnail .image', array( 'xs' => 'medium_large' ) ); ?>
-				<div class="image">
-					<?php echo wp_get_attachment_image( get_post_thumbnail_id(), 'medium_large' ); ?>
-				</div>
-			</a>
-		</div>
+	<div class="entry-header">
+
+		<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
+		<p class="entry-link">
+			<a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a>
+		</p>
+
 	</div>
 
-	<div class="entry-header">
-		<h4 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+	<div class="entry-excerpt">
+		<?php the_excerpt(); ?>
 	</div>
 
 </article>
