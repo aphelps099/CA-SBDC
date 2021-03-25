1cb3ccdbb94134123ad47df0348695f7da793c50
Cameron Rahman <cameron.rahman@gmail.com>
2021-03-25 09:30:57 -0700

    Adding network site resource syncing functionality

--- wp-content/plugins/crown-resources/classes/class-crown-resources.php
+++ wp-content/plugins/crown-resources/classes/class-crown-resources.php
@@ -30,6 +30,7 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 		public static $init = false;
 
 		public static $resource_post_type = null;
+		public static $syndicated_resource_post_type = null;
 		public static $resource_type_taxonomy = null;
 
 
@@ -41,10 +42,26 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 			register_activation_hook( $plugin_file, array( __CLASS__, 'activate' ));
 			register_deactivation_hook( $plugin_file, array( __CLASS__, 'deactivate' ));
 
+			add_action( 'init', array( __CLASS__, 'init_scheduled_events' ) );
+			add_action( 'crown_sync_resource_data', array( __CLASS__, 'sync_resource_data' ) );
+			// add_action( 'init', array( __CLASS__, 'sync_resource_data' ), 100, 0 );
+
 			add_action( 'after_setup_theme', array( __CLASS__, 'register_resource_post_type' ) );
 			add_action( 'after_setup_theme', array( __CLASS__, 'register_resource_type_taxonomy' ) );
 
+			add_action( 'save_post', array( __CLASS__, 'update_shared_post_reference' ), 100 );
+			add_action( 'after_delete_post', array( __CLASS__, 'delete_syndicated_post' ), 100 );
+
 			// add_filter( 'use_block_editor_for_post_type', array( __CLASS__, 'filter_use_block_editor_for_post_type' ), 10, 2 );
+
+			add_action( 'admin_init', array( __CLASS__, 'process_action_publish_syndicated_resource' ) );
+			add_action( 'admin_init', array( __CLASS__, 'process_action_unpublish_syndicated_resource' ) );
+			add_action( 'admin_notices', array( __CLASS__, 'output_syndicated_resource_admin_notices' ) );
+			add_filter( 'bulk_actions-edit-resource_s', array( __CLASS__, 'filter_bulk_actions_edit_resource_s'), 100, 2 );
+			add_filter( 'disable_months_dropdown', '__return_true' );
+			add_filter( 'display_post_states', array( __CLASS__, 'filter_display_post_states'), 10, 2 );
+			add_filter( 'post_row_actions', array( __CLASS__, 'filter_post_row_actions' ), 100, 2 );
+			add_filter( 'get_edit_post_link', array( __CLASS__, 'filter_get_edit_post_link' ), 10, 3 );
 
 			add_action( 'admin_enqueue_scripts', array( __CLASS__, 'register_admin_styles' ) );
 
@@ -90,7 +107,92 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 		}
 
 
+		public static function init_scheduled_events() {
+
+			// wp_clear_scheduled_hook( 'crown_sync_resource_data' );
+			if ( ! wp_next_scheduled( 'crown_sync_resource_data' ) ) {
+				wp_schedule_single_event( time() - 60, 'crown_sync_resource_data' );
+			}
+
+		}
+
+
+		public static function sync_resource_data( $sync_all = false ) {
+			global $wpdb;
+
+			$current_time = new DateTime();
+
+			if ( ! is_main_site() ) {
+
+				$query_modified_time = new DateTime( '@0' );
+				if ( ! $sync_all ) {
+					$query_modified_time = get_option( 'crown_resource_data_last_synced' ) ? new DateTime( get_option( 'crown_resource_data_last_synced', 0 ) ) : new DateTime();
+					$query_modified_time->modify( '-1 hour' );
+				}
+
+				$syn_post_ids = $wpdb->get_col( $wpdb->prepare( "
+					SELECT pm2.meta_value
+					FROM $wpdb->posts p
+					INNER JOIN $wpdb->postmeta pm1 ON (pm1.post_id = p.ID AND pm1.meta_key = '_original_site_id')
+					INNER JOIN $wpdb->postmeta pm2 ON (pm2.post_id = p.ID AND pm2.meta_key = '_original_post_id')
+					WHERE p.post_type = 'resource_s'
+						AND pm1.meta_value = %s
+				", get_main_site_id() ) );
+
+				$dest_site = get_current_blog_id();
+				switch_to_blog( get_main_site_id() );
+
+				$update_post_ids = get_posts( array(
+					'post_type' => 'resource',
+					'posts_per_page' => -1,
+					'fields' => 'ids',
+					'date_query' => array(
+						array( 'column' => 'post_modified_gmt', 'after' => $query_modified_time->format( 'Y-m-d H:i:s' ) )
+					),
+					'meta_query' => array(
+						array( 'key' => '__resource_options', 'value' => 'post-to-center-sites' )
+					)
+				) );
+
+				foreach ( $update_post_ids as $post_id ) {
+					self::syndicate_post( $post_id, $dest_site );
+				}
+
+				$post_ids = get_posts( array(
+					'post_type' => 'resource',
+					'posts_per_page' => -1,
+					'fields' => 'ids',
+					'meta_query' => array(
+						array( 'key' => '__resource_options', 'value' => 'post-to-center-sites' )
+					)
+				) );
+				
+				$old_post_ids = array_diff( $syn_post_ids, $post_ids );
+				foreach ( $old_post_ids as $post_id ) {
+					self::delete_syndicated_post( $post_id, $dest_site );
+				}
+
+				restore_current_blog();
+
+			}
+
+			update_option( 'crown_resource_data_last_synced', $current_time->format( 'Y-m-d H:i:s' ) );
+
+			$sync_interval = 30; // minutes
+			wp_clear_scheduled_hook( 'crown_sync_resource_data' );
+			wp_schedule_single_event( time() + ( 60 * $sync_interval ), 'crown_sync_resource_data' );
+
+		}
+
+
 		public static function register_resource_post_type() {
+
+			$resource_options = array();
+			if ( is_main_site() ) {
+				$resource_options[] = array( 'value' => 'post-to-center-sites', 'label' => 'Display on Center Sites' );
+			} else {
+				$resource_options[] = array( 'value' => 'do-not-post-to-regional-site', 'label' => 'Don\'t Display on Regional Site' );
+			}
 
 			self::$resource_post_type = new PostType( array(
 				'name' => 'resource',
@@ -113,10 +215,44 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 					)
 				),
 				'metaBoxes' => array(
-					
+					new MetaBox( array(
+						'id' => 'resource-options',
+						'title' => 'Resource Options',
+						'context' => 'side',
+						'fields' => array(
+							new Field( array(
+								'input' => new CheckboxSet( array( 'name' => 'resource_options', 'options' => $resource_options ) )
+							) )
+						)
+					) )
+				)
+			) );
+
+			// $count = count( get_posts( array( 'post_type' => 'resource_s', 'posts_per_page' => -1, 'fields' => 'ids', 'post_status' => 'pending' ) ) );
+			$count = 0;
+			self::$syndicated_resource_post_type = new PostType( array(
+				'name' => 'resource_s',
+				'singularLabel' => 'Syndicated Resource',
+				'pluralLabel' => 'Syndicated Resources',
+				'settings' => array(
+					'public' => false,
+					'show_in_menu' => 'edit.php?post_type=resource',
+					'show_ui' => true,
+					'labels' => array(
+						'all_items' => 'Syndicated' . ( $count ? ' <span class="awaiting-mod">' . $count . '</span>' : '' )
+					)
 				),
 				'listTableColumns' => array(
-					
+					new ListTableColumn( array(
+						'key' => 'resource-site',
+						'title' => 'SBDC',
+						'position' => 2,
+						'outputCb' => function( $post_id, $args ) {
+							$site_id = get_post_meta( $post_id, '_original_site_id', true );
+							$site_details = get_blog_details( array( 'blog_id' => $site_id ) );
+							echo '<a href="' . $site_details->siteurl . '">' . $site_details->blogname . '</a>';
+						}
+					) )
 				)
 			) );
 
@@ -129,7 +265,7 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 				'name' => 'resource_type',
 				'singularLabel' => 'Resource Type',
 				'pluralLabel' => 'Resource Types',
-				'postTypes' => array( 'resource' ),
+				'postTypes' => array( 'resource', 'resource_s' ),
 				'settings' => array(
 					'hierarchical' => true,
 					'rewrite' => array( 'slug' => 'resource-types', 'with_front' => false ),
@@ -173,8 +309,256 @@ if ( ! class_exists( 'Crown_Resources' ) ) {
 		}
 
 
+		public static function update_shared_post_reference( $post_id ) {
+
+			if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id;
+			// if ( defined( 'DOING_CRON' ) && DOING_CRON ) return $post_id;
+
+			$post = get_post( $post_id );
+			if ( ! $post || $post->post_type != 'resource' ) return $post_id;
+			if ( $post->post_title == 'Auto Draft' ) return $post_id;
+
+			$options = get_post_meta( $post_id, '__resource_options' );
+
+			if ( ! in_array( 'do-not-post-to-regional-site', $options ) && $post->post_status == 'publish' && ! is_main_site() ) {
+				self::syndicate_post( $post_id, get_main_site_id() );
+			} else {
+				self::delete_syndicated_post( $post_id, get_main_site_id() );
+			}
+
+		}
+
+
+		protected static function syndicate_post( $post_id, $dest_site ) {
+
+			$post_arr = array(
+				'post_title' => get_the_title( $post_id ),
+				'post_date' => get_the_time( 'Y-m-d H:i:s', $post_id )
+			);
+
+			$taxonomies = array(
+				'post_topic' => array(),
+				'resource_type' => array()
+			);
+			foreach ( $taxonomies as $tax => $terms ) {
+				$taxonomies[ $tax ] = wp_get_object_terms( $post_id, $tax );
+				if ( is_wp_error( $taxonomies[ $tax ] ) ) {
+					$taxonomies[ $tax ] = array();
+				} else {
+					$taxonomies[ $tax ] = array_map( function( $n ) {
+						$branch = array( clone $n );
+						while( $branch[0]->parent != 0 ) {
+							array_unshift( $branch, get_term( $branch[0]->parent, $n->taxonomy ) );
+						}
+						$n->branch = $branch;
+						return $n;
+					}, $taxonomies[ $tax ] );
+				}
+			}
+
+			$meta = array();
+			foreach ( $meta as $k => $v ) {
+				$meta[ $k ] = get_post_meta( $post_id, $k, true );
+			}
+
+			$src_site = get_current_blog_id();
+			switch_to_blog( $dest_site );
+			
+			$syn_id = get_posts( array(
+				'post_type' => 'resource_s',
+				'posts_per_page' => 1,
+				'fields' => 'ids',
+				'post_status' => 'any',
+				'meta_query' => array(
+					array( 'key' => '_original_site_id', 'value' => $src_site ),
+					array( 'key' => '_original_post_id', 'value' => $post_id )
+				)
+			) );
+			$syn_id = ! empty( $syn_id ) ? $syn_id[0] : null;
+
+			if ( ! $syn_id ) {
+				// $syn_id = wp_insert_post( array( 'post_type' => 'resource_s', 'post_status' => is_main_site() ? 'pending' : 'publish' ) );
+				$syn_id = wp_insert_post( array( 'post_type' => 'resource_s', 'post_status' => 'publish' ) );
+			}
+
+			wp_update_post( array_merge( $post_arr, array( 'ID' => $syn_id ) ) );
+
+			foreach ( $taxonomies as $tax => $terms ) {
+				$syn_term_ids = array();
+				foreach ( $terms as $term ) {
+					$syn_term_id = 0;
+					foreach ( $term->branch as $t ) {
+						$term_args = array( 'parent' => $syn_term_id );
+						if ( ( $result = term_exists( $t->name, $tax, $syn_term_id ) ) ) {
+							$syn_term_id = intval( $result['term_id'] );
+						} else if ( ( $result = @wp_insert_term( $t->name, $tax, $term_args ) ) ) {
+							if ( is_wp_error( $result ) ) break;
+							$syn_term_id = intval( $result['term_id'] );
+						}
+					}
+					if ( $syn_term_id ) $syn_term_ids[] = $syn_term_id;
+				}
+				wp_set_object_terms( $syn_id, $syn_term_ids, $tax, false );
+			}
+
+			foreach ( $meta as $k => $v ) {
+				update_post_meta( $syn_id, $k, $v );
+			}
+
+			update_post_meta( $syn_id, '_original_site_id', $src_site );
+			update_post_meta( $syn_id, '_original_post_id', $post_id );
+
+			restore_current_blog();
+
+		}
+
+
+		public static function delete_syndicated_post( $post_id, $dest_site = 0 ) {
+			if ( empty( $dest_site ) ) $dest_site = get_main_site_id();
+
+			$src_site = get_current_blog_id();
+			switch_to_blog( $dest_site );
+				
+			$syn_id = get_posts( array(
+				'post_type' => 'resource_s',
+				'posts_per_page' => 1,
+				'fields' => 'ids',
+				'post_status' => 'any',
+				'meta_query' => array(
+					array( 'key' => '_original_site_id', 'value' => $src_site ),
+					array( 'key' => '_original_post_id', 'value' => $post_id )
+				)
+			) );
+			$syn_id = ! empty( $syn_id ) ? $syn_id[0] : null;
+
+			if ( $syn_id ) {
+				wp_delete_post( $syn_id, true );
+			}
+			
+			restore_current_blog();
+
+		}
+
+
 		public static function filter_use_block_editor_for_post_type( $use_block_editor, $post_type ) {
 			return in_array( $post_type, array( 'resource' ) ) ? false : $use_block_editor;
+		}
+
+
+		public static function process_action_publish_syndicated_resource() {
+			
+			$post_id = isset( $_GET['publish_resource_s'] ) ? intval( $_GET['publish_resource_s'] ) : 0;
+			if ( ! $post_id ) return;
+			if ( ! check_admin_referer( 'publish-resource-s-' . $post_id ) ) return;
+
+			wp_update_post( array( 'ID' => $post_id, 'post_status' => 'publish' ) );
+
+			$sendback = admin_url( 'edit.php?post_type=resource_s' );
+			$sendback = add_query_arg( array( 'published_resource_s' => $post_id ), $sendback );
+			wp_redirect( $sendback );
+			die;
+
+		}
+
+
+		public static function process_action_unpublish_syndicated_resource() {
+			
+			$post_id = isset( $_GET['unpublish_resource_s'] ) ? intval( $_GET['unpublish_resource_s'] ) : 0;
+			if ( ! $post_id ) return;
+			if ( ! check_admin_referer( 'unpublish-resource-s-' . $post_id ) ) return;
+
+			wp_update_post( array( 'ID' => $post_id, 'post_status' => 'pending' ) );
+
+			$sendback = admin_url( 'edit.php?post_type=resource_s' );
+			$sendback = add_query_arg( array( 'unpublished_resource_s' => $post_id ), $sendback );
+			wp_redirect( $sendback );
+			die;
+
+		}
+
+
+		public static function output_syndicated_resource_admin_notices() {
+			$notices = array();
+
+			$post_id = isset( $_GET['published_resource_s'] ) ? intval( $_GET['published_resource_s'] ) : 0;
+			if ( $post_id ) {
+				$notices[] = array( 'type' => 'success', 'message' => __( 'The syndicated resource, "' . get_the_title( $post_id ) . '", has been published!', 'crown-resources' ) );
+			}
+
+			$post_id = isset( $_GET['unpublished_resource_s'] ) ? intval( $_GET['unpublished_resource_s'] ) : 0;
+			if ( $post_id ) {
+				$notices[] = array( 'type' => 'success', 'message' => __( 'The syndicated resource, "' . get_the_title( $post_id ) . '", has been unpublished and updated to "pending".', 'crown-resources' ) );
+			}
+
+			foreach ( $notices as $notice ) {
+				$notice = array_merge( array(
+					'type' => 'info',
+					'dismissible' => true,
+					'message' => ''
+				), $notice );
+				?>
+					<div class="notice notice-<?php echo $notice['type']; ?> <?php echo $notice['dismissable'] ? 'is-dismissible' : ''; ?>">
+						<p><?php echo $notice['message']; ?></p>
+					</div>
+				<?php
+			}
+
+		}
+
+
+		public static function filter_bulk_actions_edit_resource_s( $actions ) {
+			$actions = array();
+			return $actions;
+		}
+
+
+		public static function filter_display_post_states( $post_states, $post ) {
+			if ( $post->post_type == 'resource' && is_main_site() && in_array( 'post-to-center-sites', get_post_meta( $post->ID, '__resource_options' ) ) ) {
+				$post_states['post-syndicated'] = 'Syndicated';
+			} else if ( $post->post_type == 'resource' && ! is_main_site() && ! in_array( 'do-not-post-to-regional-site', get_post_meta( $post->ID, '__resource_options' ) ) ) {
+				$post_states['post-syndicated'] = 'Syndicated';
+			}
+			return $post_states;
+		}
+
+
+		public static function filter_post_row_actions( $actions, $post ) {
+			if ( $post->post_type == 'resource_s' ) {
+				$actions = array();
+				if ( is_main_site() ) {
+					if ( $post->post_status == 'publish' ) {
+						$actions = array(
+							'unpublish' => sprintf(
+								'<a href="%s" class="submitunpublish" aria-label="%s">%s</a>',
+								wp_nonce_url( add_query_arg( array( 'unpublish_resource_s' => $post->ID ) ), 'unpublish-resource-s-' . $post->ID ),
+								esc_attr( sprintf( __( 'Unpublish &#8220;%s&#8221;' ), $post->post_title ) ),
+								_x( 'Unpublish', 'verb' )
+							)
+						);
+					} else {
+						$actions = array(
+							'publish' => sprintf(
+								'<a href="%s" class="submitpublish" aria-label="%s">%s</a>',
+								wp_nonce_url( add_query_arg( array( 'publish_resource_s' => $post->ID ) ), 'publish-resource-s-' . $post->ID ),
+								esc_attr( sprintf( __( 'Publish &#8220;%s&#8221;' ), $post->post_title ) ),
+								_x( 'Publish', 'verb' )
+							)
+						);
+					}
+				}
+			}
+			return $actions;
+		}
+		
+
+		public static function filter_get_edit_post_link( $link, $post_id, $context ) {
+			if ( get_post_type( $post_id ) == 'resource_s' ) {
+				$original_post_id = get_post_meta( $post_id, '_original_post_id', true );
+				switch_to_blog( get_post_meta( $post_id, '_original_site_id', true ) );
+				$link = get_permalink( $original_post_id );
+				restore_current_blog();
+			}
+			return $link;
 		}
 
 
