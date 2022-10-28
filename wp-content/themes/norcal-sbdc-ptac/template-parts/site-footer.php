6814e0c5ab25eb564ab75def92d00a6791c76d1f
Sarah Blikre <sarahb@jordancrown.com>
2022-10-28 16:22:54 -0700

    Remove revslider from repo

---
+++ wp-content/themes/norcal-sbdc-ptac/template-parts/site-footer.php
@@ -0,0 +1,143 @@
+<footer id="footer" role="contentinfo">
+	<div class="inner">
+		<div class="container">
+
+			<div class="upper">
+				<div class="inner">
+
+					<div id="site-footer-regional-info">
+
+						<div class="contact">
+							<h3><?php _e( 'Questions?', 'crown_theme' ) ?> <span class="bubble"><?php _e( 'Call us.', 'crown_theme' ) ?></span></h3>
+							<?php echo do_shortcode( '[contact_info context="phone-hours"]' ); ?>
+						</div>
+
+						<nav id="footer-primary-cta-links">
+							<?php
+								wp_nav_menu( array(
+									'theme_location' => 'footer_cta_links',
+									'container' => '',
+									'menu_id' => 'footer-primary-cta-links-menu',
+									'depth' => 1,
+									'fallback_cb' => false
+								) );
+							?>
+						</nav>
+
+					</div>
+
+					<?php /* to be used in center sites ?>
+
+					<div id="site-footer-branding">
+						<div id="site-footer-title">
+							<a href="<?php echo home_url( '/' ); ?>"><?php echo get_bloginfo( 'name' ); ?></a>
+						</div>
+					</div>
+
+					<div class="footer-widget-areas">
+						<div class="inner">
+							
+							<aside id="footer-widgets-1" class="footer-widgets">
+								<?php dynamic_sidebar( 'footer-1' ); ?>
+							</aside>
+		
+							<aside id="footer-widgets-2" class="footer-widgets">
+								<?php dynamic_sidebar( 'footer-2' ); ?>
+							</aside>
+		
+							<aside id="footer-widgets-3" class="footer-widgets">
+								<?php dynamic_sidebar( 'footer-3' ); ?>
+							</aside>
+		
+							<aside id="footer-widgets-4" class="footer-widgets">
+								<?php dynamic_sidebar( 'footer-4' ); ?>
+							</aside>
+
+						</div>
+					</div>
+
+					<?php */ ?>
+
+					<div class="social-ctas">
+
+						<?php ct_social_links( array( 'title' => 'Stay Connected' ) ); ?>
+
+						<?php $google_reviews_link = get_option( 'theme_config_footer_google_reviews_link' ); ?>
+						<?php if ( ! empty( $google_reviews_link ) ) { ?>
+							<a href="<?php echo $google_reviews_link; ?>" target="_blank" class="google-reviews-link">
+								<span class="inner">Review us on Google</span>
+							</a>
+						<?php } ?>
+
+						<div id="google-translate"></div>
+						<script>
+							function googleTranslateElementInit() {
+								new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google-translate');
+							}
+						</script>
+						<script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+					</div>
+
+				</div>
+			</div>
+
+			<div class="lower">
+				<div class="inner">
+
+					<div id="site-footer-meta">
+
+						<?php $description = apply_filters( 'crown_site_footer_description', '' ); ?>
+						<?php if ( ! empty( $description ) ) { ?>
+							<div id="site-description"><?php echo apply_filters( 'the_content', $description ); ?></div>
+						<?php } ?>
+	
+						<div class="accreditations">
+							<div class="inner">
+
+								<div class="logo powered-by-sba">
+									<a href="https://www.sba.gov" target="_blank"><img src="<?php echo Crown_Theme::get_uri(); ?>/assets/img/logos/powered-by-sba.png" alt="Powered by U.S. Small Business Association"></a>
+								</div>
+
+								<div class="logo ca-gobed">
+									<a href="https://business.ca.gov" target="_blank"><img src="<?php echo Crown_Theme::get_uri(); ?>/assets/img/logos/ca-osba.png" alt="California Governor's Office of Business and Economic Development"></a>
+								</div>
+
+							</div>
+						</div>
+
+						<div class="logos">
+							<div class="inner">
+
+								<div class="logo sbdc-accredited-member">
+									<img src="<?php echo Crown_Theme::get_uri(); ?>/assets/img/logos/sbdc-accredited-member-alt.png" alt="America's SBDC Accredited Member">
+								</div>
+
+							</div>
+						</div>
+
+					</div>
+
+					<?php $copyright = apply_filters( 'crown_site_footer_copyright', '' ); ?>
+					<?php if ( ! empty( $copyright ) ) { ?>
+						<p id="site-copyright"><?php echo $copyright; ?></p>
+					<?php } ?>
+
+					<nav id="footer-legal-links">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'footer_legal_links',
+								'container' => '',
+								'menu_id' => 'footer-legal-links-menu',
+								'depth' => 1,
+								'fallback_cb' => false
+							) );
+						?>
+					</nav>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+</footer>
---
+++ wp-content/themes/norcal-sbdc-ptac/template-parts/site-pre-footer.php
@@ -0,0 +1,41 @@
+<div id="pre-footer">
+	<div class="inner">
+		<div class="container">
+
+			<div id="site-footer-header"><?php echo get_bloginfo( 'name' ); ?></div>
+
+			<div class="contents">
+
+				<?php $form_id = get_option( 'theme_config_footer_subscribe_form' ); ?>
+				<?php if ( ! empty( $form_id ) && function_exists( 'gravity_form' ) ) { ?>
+					<div id="footer-subscribe-form">
+						<h6 class="form-overtitle"><span><?php _e( 'SBDC', 'crown_theme' ); ?></span><?php _e( 'Intel&trade;', 'crown_theme' ); ?></h6>
+						<?php gravity_form( $form_id, true, true, false, null, true, 1000000, true ); ?>
+					</div>
+				<?php } ?>
+
+				<div id="site-footer-logos">
+
+					<?php $host_logos = get_repeater_entries( 'blog', 'theme_config_footer_host_logos' ); ?>
+					<?php if ( ! empty( $host_logos ) ) { ?>
+						<div id="site-footer-host-logos">
+							<p class="intro"><?php _e( 'Our program is proudly hosted by:', 'crown_theme' ); ?></p>
+							<div class="logos">
+								<?php foreach( $host_logos as $logo ) { ?>
+									<div class="logo">
+										<?php if ( ! empty( $logo['link_url'] ) ) { ?><a href="<?php echo esc_attr( $logo['link_url'] ); ?>" target="_blank"><?php } ?>
+										<?php echo wp_get_attachment_image( $logo['image'], 'medium' ); ?>
+										<?php if ( ! empty( $logo['link_url'] ) ) { ?></a><?php } ?>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					<?php } ?>
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+</div>
